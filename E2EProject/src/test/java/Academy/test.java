c5c6470828966bb71791f872bf3fe97f1dc609b9
Monali <monalijain01@gmail.com>
2020-09-27 21:36:42 -0500

    My first commit

---
+++ E2EProject/src/test/java/Academy/test.java
@@ -0,0 +1,16 @@
+package Academy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://qaclickacademy.com");
+		// TODO Auto-generated method stub
+
+	}
+
+}
